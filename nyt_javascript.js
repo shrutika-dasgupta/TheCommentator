6711572bca34312511399c97143b81d2a5bfa875
Shrutika Dasgupta <sd2841@columbia.edu>
2014-11-09 00:44:03 -0500

    Successfully removed the error | Random button still to function

--- nyt_javascript.js
+++ nyt_javascript.js
@@ -1,18 +1,67 @@
-function get_form_data(commentator_name, article_name)
-{
-  console.log(commentator_name);
-  console.log(article_name);
-}
-
 parameterList = {
   'api-key': '71b688b6fcaffe9ac59413b1d7de1a0c:1:70151851'
 }
 
-function get_random_comments()
+function get_form_data(commentator_name, article_name, date)
 {
-  console.log("random button clicked");
-  $.ajax({
-    url: 'http://api.nytimes.com/svc/community/v2/comments/random.json',
+  console.log(commentator_name);
+  console.log(article_name);
+  console.log(date);
+
+  if(date != '')
+  {
+    var date_array = date.split('/');
+    if(date_array.length != 3)
+    {
+      console.log("Invalid Date");
+    }
+    else
+    {
+      var year = date_array[0];
+      var month = date_array[1];
+      var day = date_array[2];
+
+      var date_str = year + month + day;
+
+      if(year.length != 4 || month.length != 2 || day.length != 2)
+      {
+        console.log("invalid-date format");
+      }
+      else
+      {
+        console.log(date_str);
+
+        $.ajax({
+          url: ('http://api.nytimes.com/svc/community/v2/comments/by-date/'+date_str+'.json'),
+          data: parameterList,
+          dataType: 'json',
+          method: 'GET',
+          complete : function(data){
+            console.log(data);
+          },
+          success: function(){
+            console.log("success");
+          },
+          fail: function(){
+            console.log("error");
+          }
+        });
+      }
+      
+    }
+  }
+
+  if(article_name != '')
+  {
+
+  }
+
+  if(commentator_name != '')
+  {
+
+  }
+  /*$.ajax({
+    url: '',
     data: parameterList,
     dataType: 'jsonp',
     complete : function(){
@@ -24,7 +73,33 @@ function get_random_comments()
     error: function(){
       console.log("error");
     }
+  });*/
+}
+
+function get_random_comments()
+{
+  console.log("random button clicked");
+  $.ajax({
+    type:'POST',
+    url: 'http://api.nytimes.com/svc/community/v2/comments/random.json',
+    data: parameterList,
+    cache: true,
+    contentType:'application/json',
+    dataType: 'jsonp',
+    crossDomain: true,
+    jsonp:false,
+    complete : function(data){
+      console.log(data);
+      console.log("complete");
+    },
+    success: function(){
+      console.log("success");
+    },
+    error: function(){
+      console.log("error");
+    }
   });
+  //console.log(results);
 }
 
 $(document).ready(function(){
@@ -35,6 +110,6 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("#search-query").click(function(){
-    get_form_data($('#commentator-name').val(), $('#article-name').val())
+    get_form_data($('#commentator-name').val(), $('#article-name').val(), $('#datepicker1').val())
   });
 });
